eb95df903cfcb86f97ad312ba0d2e3a938b7dc0f
ynt29tie <ynt29tie@gmail.com>
2021-10-20 02:37:49 +0700

    update 20/10/2021

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="test">
+    <Nav></Nav>
+    <Content></Content>
+    <Nav></Nav>
+    <Content></Content>
+    <h3>Footer</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue";
+import Content from "./components/Content.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Nav,
+    Content,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +29,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.test {
+  display: flex;
+  height: 100vh;
+  background-color: red;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <h2 class="ct">Content</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Content",
+};
+</script>
+<style>
+.ct {
+  background-color: black;
+}
+</style>
